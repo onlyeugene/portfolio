22d1274c97bc3dc00d8d85e6f1175762c26384ad
onlyeugene <102293184+onlyeugene@users.noreply.github.com>
2025-01-16 13:08:42 +0100

    updated codes

--- components/header.tsx
+++ components/header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="border-b border-gray-800 py-2 px-10 pr-48 bg-[#1c1c1c] flex fixed min-w-full gap-5 justify-between">
       <div>
-      {/* <UserRoutes /> */}
+      <UserRoutes />
       </div>
      <div className="flex items-center gap-4">
      <UserButtons />
